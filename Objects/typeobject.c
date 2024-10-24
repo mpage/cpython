b33986abaaf1094c8d0b90e51e2bdad64eb997c5
Matt Page <mpage@meta.com>
2024-10-24 15:38:23 -0700

    Make CALL_ALLOC_AND_ENTER_INIT thread-safe
    - Modify `get_init_for_simple_managed_python_class` to return both init
      as well as the type version at the time of lookup.
    - Modify caching logic to verify that the current version of the type
      matches the version at the time of lookup. This prevents potentially
      caching a stale value if we race with an update to __init__.
    - Only cache __init__ functions that are deferred in free-threaded builds.
      This ensures that the borrowed reference to __init__ that is stored in
      the cache is valid if the type version guard in _CHECK_AND_ALLOCATE_OBJECT
      passes:
      1. The type version is cleared before the reference in the MRO to __init__
         is destroyed.
      2. If the reference in (1) was the last reference then the __init__ method
         will be queued for deletion the next time GC runs.
      3. GC requires stopping the world, which forces a synchronizes-with operation
         between all threads.
      4. If the GC collects the cached __init__, then type's version will have been
         updated *and* the update will be visible to all threads, so the guard
         cannot pass.
    - There are no escaping calls in between loading from the specialization cache
      and pushing the frame. This is a requirement for the default build.

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -5643,6 +5643,27 @@ _PyType_Lookup(PyTypeObject *type, PyObject *name)
     return res;
 }
 
+
+int
+_PyType_CacheInitForSpecialization(PyTypeObject *type, PyObject *init,
+                                   unsigned int tp_version)
+{
+    if (!init || !tp_version) {
+        return 0;
+    }
+    int can_cache = type->tp_version_tag == tp_version;
+    BEGIN_TYPE_LOCK();
+    #ifdef Py_GIL_DISABLED
+    can_cache = can_cache && _PyObject_HasDeferredRefcount(init);
+    #endif
+    if (can_cache) {
+        PyHeapTypeObject *ht = (PyHeapTypeObject*) type;
+        FT_ATOMIC_STORE_PTR_RELAXED(ht->_spec_cache.init, init);
+    }
+    END_TYPE_LOCK();
+    return can_cache;
+}
+
 static void
 set_flags(PyTypeObject *self, unsigned long mask, unsigned long flags)
 {
