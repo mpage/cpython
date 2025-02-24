88875ee76ac9b1160b9482af93045ae975a2237e
Matt Page <mpage@meta.com>
2025-02-24 12:10:27 -0800

    Don't promote borrowed references in STORE_FAST
    This reduces the number of changes to the interpreter loop and leaves
    the various STORE_FAST instructions unmodified. It means that we cannot
    optimize LOAD_FAST instructions that are consumed by STORE_FAST instructions,
    but I think this is a net positive. It speeds up STORE_FAST and doesn't appreciably
    reduce the number of optimized instructions in the benchmark suite.

--- Python/generated_cases.c.h
+++ Python/generated_cases.c.h
@@ -10967,7 +10967,7 @@
             _PyStackRef value;
             value = stack_pointer[-1];
             _PyStackRef tmp = GETLOCAL(oparg);
-            GETLOCAL(oparg) = _PyStackRef_StealIfUnborrowed(value);
+            GETLOCAL(oparg) = value;
             stack_pointer += -1;
             assert(WITHIN_STACK_BOUNDS());
             _PyFrame_SetStackPointer(frame, stack_pointer);
@@ -10990,7 +10990,7 @@
             uint32_t oparg1 = oparg >> 4;
             uint32_t oparg2 = oparg & 15;
             _PyStackRef tmp = GETLOCAL(oparg1);
-            GETLOCAL(oparg1) = _PyStackRef_StealIfUnborrowed(value1);
+            GETLOCAL(oparg1) = value1;
             value2 = PyStackRef_DUP(GETLOCAL(oparg2));
             stack_pointer[-1] = value2;
             _PyFrame_SetStackPointer(frame, stack_pointer);
@@ -11014,14 +11014,14 @@
             uint32_t oparg1 = oparg >> 4;
             uint32_t oparg2 = oparg & 15;
             _PyStackRef tmp = GETLOCAL(oparg1);
-            GETLOCAL(oparg1) = _PyStackRef_StealIfUnborrowed(value1);
+            GETLOCAL(oparg1) = value1;
             stack_pointer += -1;
             assert(WITHIN_STACK_BOUNDS());
             _PyFrame_SetStackPointer(frame, stack_pointer);
             PyStackRef_XCLOSE(tmp);
             stack_pointer = _PyFrame_GetStackPointer(frame);
             tmp = GETLOCAL(oparg2);
-            GETLOCAL(oparg2) = _PyStackRef_StealIfUnborrowed(value2);
+            GETLOCAL(oparg2) = value2;
             stack_pointer += -1;
             assert(WITHIN_STACK_BOUNDS());
             _PyFrame_SetStackPointer(frame, stack_pointer);
