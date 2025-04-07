521fae649b7c023c883315ca4f29e314b0ec28f2
Matt Page <mpage@meta.com>
2025-04-07 12:01:48 -0700

    Put load bearing store back

--- Include/internal/pycore_interpframe.h
+++ Include/internal/pycore_interpframe.h
@@ -171,9 +171,7 @@ _PyFrame_GetStackPointer(_PyInterpreterFrame *frame)
 {
     assert(frame->stackpointer != NULL);
     _PyStackRef *sp = frame->stackpointer;
-#ifndef NDEBUG
     frame->stackpointer = NULL;
-#endif
     return sp;
 }
 
